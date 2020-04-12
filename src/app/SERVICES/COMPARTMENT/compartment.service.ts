74c92c00f1ed109aa4a940c05512e0b697e69c7c
Laura Talom <talom.laura@gmail.com>
2020-04-12 14:06:03 +0100

    final-changes

--- src/app/SERVICES/COMPARTMENT/compartment.service.ts
+++ src/app/SERVICES/COMPARTMENT/compartment.service.ts
@@ -1,9 +1,21 @@
+import { GET_COMPARTMENT_URL } from './../../TOOLS/FUNCTIONS/Url';
+import { Compartment } from './../../MODELS/Compartment';
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { LocalStorageService } from '../STORAGE/local-storage.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompartmentService {
 
-  constructor() { }
+  constructor(
+    private localstore: LocalStorageService,
+    private http: HttpClient
+  ) { }
+
+  getCompartments(): Observable<Compartment[]>  {
+    return this.http.get<Compartment[]>(GET_COMPARTMENT_URL);
+   }
 }
